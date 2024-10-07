3639ac86ba9fca2ab54d32018f1cf468f9970c6f
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-10-06 23:42:45 -0300

    se definen las tareas para validación del form

--- js/registro.js
+++ js/registro.js
@@ -1,25 +1,34 @@
 
 
-// Este archivo esta pensado para ir dejando sentado las funciones y validaciones que se hara y controlara desde JS 
+// *Este archivo esta pensado para ir dejando sentado las funciones y validaciones que se hara y controlara desde JS 
 
-// 1. Cantidad de letras para Nombre   -  min 2 max 25
+// TODO 1. Cantidad de letras para Nombre   -  min 2 max 25
+// TODO 2. No aceptar espacios dento del input Nombre
+// TODO 3. Cantidad de letras para Apellido -  min 2  max 25
+// TODO 4. No aceptar espacios dento del input Apellido
 
-// 2. Cantidad de letras para Apellido -  min 2  max 25
 
-// 3 .Si me ubico sobre el input posterior sin completar un anterior o anteriores mostrar un simbolo de advertencia en el input
+// TODO 5. Email incompleto "completar"
+// TODO 6. Email "falta @"
+// TODO 7. Email "falta . (punto)"
+// TODO 8. Email "falta dominio, .com .net"
 
-// 4. Validar que solo solo pueda elegir hasta 2 checkbox, o uno solo que seria general. 
+// TODO 9. Seleccionar obligatoriamente genero.
 
-//  5. 
+// TODO 10.  contraseña  1 Mayuscula
+// TODO 11.  contraseña  1 Minuscula
+// TODO 12.  contraseña  1 Numero
+// TODO 13. contraseña  1 Caracter
+// TODO 14.  contraseña  max 12 digitos
+// TODO 15. contraseña  min 8  digitos
+// TODO 16. contraseña  pendientes en Amarillo
+// TODO 17. contraseña  completados en Verde
 
-//
+// TODO 18. Si no selecciono un tema "Seleccionar al menos una opción"
+// TODO 19. Validar que solo solo pueda elegir hasta 2 checkbox, o uno solo que seria general. 
+// TODO 20. Si no selecciono un rango de EDAD "Seleccionar un rango de EDAD"
 
-//
+// TODO 21 . Si dejo el input vacio Ó incompleto "completar" Ó ANIMACIÓN
+// TODO 22. Si completo con el minimo requerido "Valído" o icono de check en verde.
+// TODO 23. Si todos los inputs estan completados con el minimo requerido se puede enviar el formulario. 
 
-//
-
-//
-
-//
-
-//
