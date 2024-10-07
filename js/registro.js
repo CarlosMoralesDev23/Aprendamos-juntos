a8c417f3826d32c7978b1d78e112b6e6476ba3e7
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-10-06 22:53:18 -0300

    definiendo modificaciones pendietes del form

---
+++ js/registro.js
@@ -0,0 +1,25 @@
+
+
+// Este archivo esta pensado para ir dejando sentado las funciones y validaciones que se hara y controlara desde JS 
+
+// 1. Cantidad de letras para Nombre   -  min 2 max 25
+
+// 2. Cantidad de letras para Apellido -  min 2  max 25
+
+// 3 .Si me ubico sobre el input posterior sin completar un anterior o anteriores mostrar un simbolo de advertencia en el input
+
+// 4. Validar que solo solo pueda elegir hasta 2 checkbox, o uno solo que seria general. 
+
+//  5. 
+
+//
+
+//
+
+//
+
+//
+
+//
+
+//
